76ef12ecdc3d12ef6f6d40a2a21bcdf76bf2e42f
deshiyan1010 <56188225+deshiyan1010@users.noreply.github.com>
2020-06-12 14:23:13 +0530

    New

---
+++ app.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template, request, jsonify
+from plot import plot
+import json
+import plotly
+app = Flask(__name__,static_url_path='/static')
+
+def mon_name_num(month):
+    mon_dict = {"January":1,"February":2,"March":3,
+              "April":4,"May":5,"June":6,"July":7,
+              "August":8,"September":9,"October":10,
+              "November":11,"December":12}
+    return mon_dict[month]
+
+def state_code(state):
+    states = {"Total India":"TT",	"Andaman and Nicobar Islands":"AN",	"Andra Pradesh":"AP", 
+              "Arunachal Pradesh":"AR",	"Assam":"AS", "Bihar":"BR",
+              "Chandigarh":"CH", "Chhattisgarh":"CT", "Dadra and Nagar Haveli":"DN",	
+              "Daman and Diu":"DD",	"Delhi":"DL", "Goa":"GA",
+              "Gujarat":"GJ", "Haryana":"HR", "Himachal Pradesh":"HP", 
+              "Jammu and Kashmir":"JK",	"Jharkhand":"JH", "Karnataka":"KA",
+              "Kerala":"KL", "Ladakh":"LA",	"Lakshadweep":"LD",	"Madhya Pradesh":"MP",	
+              "Maharashtra":"MH", "Manipur":"MN",
+              "Meghalaya":"ML",	"Mizoram":"MZ",	"Nagaland":"NL", "Odisha":"OR",	
+              "Puducherry":"PY", "Punjab":"PB",
+              "Rajasthan":"RJ",	"Sikkim":"SK", "Tamil Nadu":"TN", "Telangana":"TG",	
+              "Tripura":"TR",	"Uttar Pradesh":"UP",
+              "Uttarakhand":"UT", "West Bengal":"WB"}
+    return states[state]
+
+@app.route('/', methods=['GET', 'POST']) # To render Homepage
+def home_page():
+    states = ["Total India","Andaman and Nicobar Islands","Andra Pradesh",
+              "Arunachal Pradesh","Assam","Bihar",
+              "Chandigarh",	"Chhattisgarh","Dadra and Nagar Haveli",
+              "Daman and Diu","Delhi","Goa",
+              "Gujarat","Haryana","Himachal Pradesh","Jammu and Kashmir",
+              "Jharkhand","Karnataka",
+              "Kerala",	"Ladakh","Lakshadweep","Madhya Pradesh","Maharashtra",
+              "Manipur","Meghalaya","Mizoram","Nagaland","Odisha","Puducherry",
+              "Punjab",
+              "Rajasthan","Sikkim",	"Tamil Nadu","Telangana","Tripura",	"Uttar Pradesh",
+              "Uttarakhand", "West Bengal"]
+    months = ["January","February","March",
+              "April","May","June","July",
+              "August","September","October",
+              "November","December"]
+    dates = [str(x) for x in range(1,32)]
+    years = ["2020","2021"]
+    types = ["Confirmed","Recovered","Deceased"]
+    cum_das = ["Cumulative","Daily"]
+    return render_template('index.html',states=states,
+                           months=months,
+                           dates=dates,
+                           years=years,
+                           types=types,
+                           cum_das=cum_das)
+
+@app.route('/math', methods=['POST'])  # This will be called from UI
+def math_operation():
+    if (request.method=='POST'):
+        sttNM = request.form['state']
+        statex = state_code(sttNM)
+        #month = mon_name_num(request.form['month'])
+        #date = request.form['date']
+        #year = request.form['year']
+        typex = request.form['type']
+        datex = request.form['datex']
+        cum_dax = request.form['cum_da']
+        #data =plot(cat=str(typex),state=state,till=str(year)+"-"+str(month)+"-"+str(date))
+        data =plot(cat=str(typex),state=statex,till=str(datex),cum_da=str(cum_dax),stt_name=sttNM)
+
+        #graphJSON = json.dumps(data,cls=plotly.utils.PlotlyJSONEncoder)
+
+        #num1=int(request.form['num1'])
+        #num2 = int(request.form['num2'])
+
+        return render_template('file.html')#,graphJSON=graphJSON
+
+
+
+if __name__ == '__main__':
+    #app.run(debug=True)
+
+    app.run(host='0.0.0.0',port=8090)
---
+++ plot.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import io
+import requests
+import matplotlib.pyplot as plt
+from sklearn.linear_model import SGDClassifier
+import random
+import numpy as np
+from sklearn.metrics import accuracy_score
+import sys
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn import linear_model
+import warnings
+import json
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+warnings.simplefilter("ignore")
+# import plotly.express as px
+
+def get_data():
+    url = "https://api.covid19india.org/csv/latest/state_wise_daily.csv"
+    s = requests.get(url).content
+    csv = pd.read_csv(io.StringIO(s.decode("utf-8")))
+
+    csv["Date"] = pd.to_datetime(csv["Date"])
+    default_start_point = pd.Timestamp("2020-03-14 00:00:00")
+    csv["Day num"] = csv["Date"] - default_start_point
+
+    conf_csv = csv[csv["Status"] == "Confirmed"].drop(["Status"], axis=1)
+    conf_csv["Day num"] = conf_csv["Day num"].dt.days
+
+    rec_csv = csv[csv["Status"] == "Recovered"].drop(["Status"], axis=1)
+    rec_csv["Day num"] = rec_csv["Day num"].dt.days
+
+    des_csv = csv[csv["Status"] == "Deceased"].drop(["Status"], axis=1)
+    des_csv["Day num"] = des_csv["Day num"].dt.days
+
+    return conf_csv, rec_csv, des_csv
+
+def to_days(ts):
+
+  num_days = (ts - pd.Timestamp("2020-03-14 00:00:00")).days
+  return num_days
+
+def to_date(num_lst):
+  
+  date_lst = []
+  for x in num_lst:
+    date_lst.append(pd.Timestamp("2020-03-14 00:00:00")+pd.to_timedelta(x,unit = "D"))
+  return date_lst
+
+
+def plot(cat="Confirmed",state="TT",till = "2020-07-01",cum_da = "Cumulative",stt_name="X"):
+  till=pd.Timestamp(str(till)+" 00:00:00")
+  num_days = to_days(till)
+  conf_csv, rec_csv, des_csv = get_data()
+
+  if cat=="Recovered":
+    X_train, y_train = np.array(rec_csv["Day num"]), np.array(rec_csv[state])
+
+  if cat=="Desceased":
+    X_train, y_train = np.array(des_csv["Day num"]), np.array(des_csv[state])
+
+  if cat=="Confirmed":
+    X_train, y_train = np.array(conf_csv["Day num"]), np.array(conf_csv[state])
+
+  if cum_da=="Cumulative":
+    y_train = y_train.cumsum()
+
+  X = X_train.reshape(-1,1)
+  vector = y_train.reshape(-1,1)
+
+
+  poly = PolynomialFeatures(degree=3)
+  X_ = poly.fit_transform(X)
+
+  clf = linear_model.Ridge()
+  clf.fit(X_, vector)
+
+
+  num_days_lst = np.array(list(range(1,num_days+1)))
+  a = poly.fit_transform(num_days_lst.reshape(-1,1))
+  # plt.plot(num_days_lst, (clf.predict(a)),"r")
+  # plt.plot(X, vector,"b")
+  # plt.show()
+
+  Predicted = go.Scatter(
+      x=to_date(num_days_lst.ravel()),
+      y=clf.predict(a).ravel(),name="Predicted"
+  )
+  Actual = go.Scatter(
+      x=to_date(X.ravel()),
+      y=vector.ravel(), name="Actual"
+  )
+  data = [Predicted,Actual]
+  plotly.offline.plot({"data":data,"layout":go.Layout(title="Covid Predictor: "+str(cat)+" cases of "+str(stt_name))},filename="templates/file.html",auto_open=False)
+  #py.iplot([trace0,trace1])
+  #trace0 = px.line(vector.ravel(),X.ravel())
+  #trace1 = px.line(clf.predict(a).ravel(),num_days_lst.ravel())
+
+  #return [trace0,trace1]
+
+
+if __name__=="__main__":
+    plot(cat="Confirmed",state="TN",till = "2020-07-01")
